6cc4749ca19b44ab6e24c33b7c6eb143726f6a4e
Matthias Beyer <mail@beyermatthias.de>
2016-09-02 11:10:42 +0200

    Add XOrVec type

--- src/ops/xor.rs
+++ src/ops/xor.rs
@@ -27,3 +27,50 @@ impl<T, U> XOr<T, U> {
 }
 
 impl_operators!(XOr, self e { self.a.filter(e) ^ self.b.filter(e) }, T, U);
+
+pub struct XOrVec<I>(Vec<Box<I>>);
+
+impl<I> XOrVec<I> {
+
+    pub fn new(i: Vec<Box<I>>) -> XOrVec<I> {
+        XOrVec(i)
+    }
+
+    // convenient function to build a XOrVec from a Filter using Filter::xor_vec()
+    //
+    // Using this function, one can:
+    //
+    // ```ignore
+    //  some_filter.xor_vec()
+    //      .push(another)
+    //      .push(filter)
+    //      .push(which)
+    //      .push(filters)
+    //      .or(something_else)
+    //      .filter(&element)
+    //  ```
+    pub fn push(&mut self, i: I) -> &mut Self {
+        self.0.push(Box::new(i));
+        self
+    }
+
+}
+
+impl<I, F: Filter<I>> Filter<I> for XOrVec<Box<F>> {
+
+    fn filter(&self, e: &I) -> bool {
+        let mut b = false;
+        for f in self.0.iter() {
+            if f.filter(e) {
+                if b { // if there is already a filter which returned true, xor yields false
+                    return false;
+                } else { // if there is no filter yet that yielded true, we can toggle the flag
+                    b = true;
+                }
+            }
+        }
+        return b
+    }
+
+}
+
